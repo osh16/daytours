3c2f29d04e0abfeb3dbd417b4b20bf9e0847762a
Oskar Hoskuldsson <osh16@hi.is>
2020-03-25 17:01:56 +0000

    breytti db.sql og readme.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # daytours
+Til að sækja project:
+$ git clone https://github.com/osh16/daytours.git
+
+Ef vesen er með gagnagrunn:
+$ rm daytours.db; sqlite3 daytours.db ".read db.sql"
--- db.sql
+++ db.sql
@@ -1,12 +1,14 @@
-create table tours (
+/*create table tours (
     
 );
 
 create table users (
 
 );
+*/
 
 create table ratings (
+    id integer primary key autoincrement not null,
     name varchar(128),
     rating_date date,
     stars integer,
@@ -15,10 +17,8 @@ create table ratings (
 
 
 /* dummy gogn fyrir junit profun */
-insert into ratings values("vofflur a borgarnesi", date('2001-04-04'), 5, "aedislega gaman hehe");
-insert into ratings values("vaendiskaup i keflavik", date('2019-12-24'), 4, "mjog flottar
-konur bara gaman sko");
-insert into ratings values("sundferd a selfossi", date('2020-01-01'), 5, "gott ad fara i sund");
-insert into ratings values("rutuferd a hlemm square", date('2019-01-07'), 3, "guide taladi ekki
-islensku");
-insert into ratings values("bjor a sjallanum i AK", date('1997-10-07'), 2, "ekki gaman");
+insert into ratings values(null,"vofflur a borgarnesi", date('2001-04-04'), 5, "aedislega gaman hehe");
+insert into ratings values(null,"vaendiskaup i keflavik", date('2019-12-24'), 4, "mjog flottar konur bara gaman sko");
+insert into ratings values(null,"sundferd a selfossi", date('2020-01-01'), 5, "gott ad fara i sund");
+insert into ratings values(null,"rutuferd a hlemm square", date('2019-01-07'), 3, "guide taladi ekki islensku");
+insert into ratings values(null,"bjor a sjallanum i AK", date('1997-10-07'), 2, "ekki gaman");
