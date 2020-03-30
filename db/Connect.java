d9c1a70dfe10c8b85ab7e0c0526780110d5c8dbf
osh16 <osh16@hi.is>
2020-03-30 19:14:54 +0000

    afram ad fikta med connect

--- db/Connect.java
+++ db/Connect.java
@@ -1,29 +1,23 @@
 import java.sql.*;
 
 public class Connect {
-    public static void connect(String query) {
-	Connection conn = null;
-	Statement stmt = null;
-	PreparedStatement pstmt = null;
-	ResultSet r = null;
+    public static void connect(String query) throws Exception {
+	Connection conn;
 
 	try {
 	    Class.forName("org.sqlite.JDBC");
 	    conn = DriverManager.getConnection("jdbc:sqlite:daytours.db");
-	    stmt = conn.createStatement();
-	    r = stmt.executeQuery(query);
-	    if (r.next()) System.out.println(r.getDouble(1));
-	    
-	} catch (SQLException e) {
+	} catch (Exception e) {
 	    e.printStackTrace();
-	} finally {
-	    if (r != null) r.close();
-	    if (stmt != null) stmt.close();
-	    if (conn != null) conn.close();
-	}
+	}     
     }
 
     public static void main(String[] args) {
-	connect("select avg(stars) from ratings");
+	try {
+	    connect("select avg(stars) from ratings");
+	} catch(Exception e) {
+	    e.printStackTrace();
+	}
+
     }
 }
