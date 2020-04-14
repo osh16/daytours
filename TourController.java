1ae3e0ce82ee40781b07521d0a7a00754f443d49
vsv8 <vsv8@hi.is>
2020-04-14 02:10:38 +0000

    kengbilað samt

--- TourController.java
+++ TourController.java
@@ -20,6 +20,71 @@ public class TourController {
 		return getTour(query);
     }
 
+    public int getCount(String query) {
+	ResultSet rs = null;
+	int count = 0;
+	try {
+	    c.connect();
+	    rs = c.retrieve(query);
+	    if (rs != null) {
+		while (rs.next()) {
+		    count++; 
+		}
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	return count;
+    }
+
+    public Tour getCheapTours(double max) {
+    	ResultSet rs = null;
+    	String query = "select * from tours where price <= " + max;
+    	// String count = "select count(*) from tours where price <= " + max;
+    	// int toursLength = Integer.parseInt(count);
+    	int toursLength = getCount(query);
+    	Tour[] tours = new Tour[toursLength];
+    	int i = 0;
+
+    	try {
+		    c.connect();
+		    rs = c.retrieve(query);
+
+	    try {
+	    	c.connect();
+	    	rs = c.retrieve(query);
+
+	    	if (rs != null) {
+	    		while (rs.next()) {
+	    			tours[i] = new Tour(
+	    				rs.getInt(1),
+	    				rs.getString(2),
+	    				rs.getString(3),
+	    				rs.getDouble(4),
+	    				rs.getString(5),
+	    				rs.getString(6)
+	    			);
+	    			i++;
+	    		}
+	    	}
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    }
+
+    	return tours;
+    }
+
+    public static void print(Tour tour) {
+    	if (tour != null) {
+    		System.out.println(tour.getId());
+			System.out.println(tour.getName());
+			System.out.println(tour.getDate());
+			System.out.println(tour.getPrice());
+			System.out.println(tour.getType());
+			System.out.println(tour.getLocation());
+    	}
+    }
+
     public Tour getTour(String query) {
 		Tour tour = null;
 		ResultSet rs = null;
@@ -70,36 +135,34 @@ public class TourController {
     	TourController tc = new TourController();
 
     	Tour tour = tc.getTourById(2);
-    	if (tour != null) {
-    		System.out.println(tour.getId());
-			System.out.println(tour.getName());
-			System.out.println(tour.getDate());
-			System.out.println(tour.getPrice());
-			System.out.println(tour.getType());
-			System.out.println(tour.getLocation());
+    	print(tour);
+
+    	Tour[] tours = tc.getCheapTours(30000);
+    	for (int i = 0; i < tours.length; i++) {
+    		print(tours[i]);
     	}
 
-    	tour = tc.getTourById(1);
-    	if (tour != null) {
-    		System.out.println(tour.getId());
-			System.out.println(tour.getName());
-			System.out.println(tour.getDate());
-			System.out.println(tour.getPrice());
-			System.out.println(tour.getType());
-			System.out.println(tour.getLocation());
-    	}
+   //  	tour = tc.getTourById(1);
+   //  	if (tour != null) {
+   //  		System.out.println(tour.getId());
+			// System.out.println(tour.getName());
+			// System.out.println(tour.getDate());
+			// System.out.println(tour.getPrice());
+			// System.out.println(tour.getType());
+			// System.out.println(tour.getLocation());
+   //  	}
 
-    	Tour newTour = new Tour(6,"allahu akhbar","2001-09-11",1.0,"hryðjuverkaferð","NY");
-    	tc.addTour(newTour);
-    	tour = tc.getTourById(6);
-    	if (tour != null) {
-    		System.out.println(tour.getId());
-			System.out.println(tour.getName());
-			System.out.println(tour.getDate());
-			System.out.println(tour.getPrice());
-			System.out.println(tour.getType());
-			System.out.println(tour.getLocation());
-    	}
+   //  	Tour newTour = new Tour(6,"allahu akhbar","2001-09-11",1.0,"hryðjuverkaferð","NY");
+   //  	tc.addTour(newTour);
+   //  	tour = tc.getTourById(6);
+   //  	if (tour != null) {
+   //  		System.out.println(tour.getId());
+			// System.out.println(tour.getName());
+			// System.out.println(tour.getDate());
+			// System.out.println(tour.getPrice());
+			// System.out.println(tour.getType());
+			// System.out.println(tour.getLocation());
+   //  	}
 
     	int maxId = tc.getLatestId();
     	System.out.println("ID = " + maxId);
