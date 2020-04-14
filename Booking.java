c0afa492ab3e2f7c00db152c397894f52b0eb40b
ths251 <54596097+ths251@users.noreply.github.com>
2020-04-14 13:40:31 +0000

    Update Booking.java
    Bætti við breytum

--- Booking.java
+++ Booking.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Booking;
-
-
 public class Booking {
     private int ID;
     private String Trip;
     private String PaymentMethod;
     private String CustomerName;
+    private int Amount;
+    private String Booking_date;
     
     
     
@@ -33,11 +27,24 @@ public class Booking {
         
         return CustomerName;
     }
+
+    public int getAmount(){
+	
+	return Amount;
+    }
+
+    public String getDate(){
+	
+	return Booking_date;
+    }
+
     
-    public Booking(int ID,String Trip,String PaymentMethod,String CustomerName){
+    public Booking(int ID,String Trip,String PaymentMethod,String CustomerName,int Amount,String Booking_date){
         this.ID = ID;
         this.Trip = Trip;
         this.PaymentMethod = PaymentMethod;
         this.CustomerName = CustomerName;
+        this.Amount = Amount;
+        this.Booking_date = Booking_date;
     }
 }
